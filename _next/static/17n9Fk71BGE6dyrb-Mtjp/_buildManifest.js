c936d3fce9edcda6e707d229389ae640f3bce4c0
Muyuan Li (from Dev Box) <muyuanli@microsoft.com>
2026-08-18 14:02:28 +0800

    Revert "Update preview after action UX changes"
    This reverts commit ca596302cc1a3df314523dec5492c7a86f15aa02.

---
+++ _next/static/17n9Fk71BGE6dyrb-Mtjp/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/powertoys-pulse-actions//_next/:path+",
+        "destination": "/powertoys-pulse-actions/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
